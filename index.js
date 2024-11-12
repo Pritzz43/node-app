1f2fb3744687ef6bcd2534c892062c88de34818e
Pritzz43 <Pritamshah888@gmail.com>
2024-11-12 15:09:35 +0530

    changes in index file

--- index.js
+++ index.js
@@ -14,5 +14,4 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is running on port 5050');
   console.log('Server is running on port 5000');
-  console.log('Server is running on port 5000');
 })
