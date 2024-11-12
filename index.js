22e9aa7325c2bc777b02ccb77ba8e2cb8f69457f
Pritzz43 <Pritamshah888@gmail.com>
2024-11-12 14:47:30 +0530

    added

--- index.js
+++ index.js
@@ -14,4 +14,5 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is running on port 5050');
   console.log('Server is running on port 5000');
+  console.log('Server is running on port 5000');
 })
